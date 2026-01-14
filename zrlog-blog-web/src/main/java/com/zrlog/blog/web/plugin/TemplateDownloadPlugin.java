3cf0fa6a691682bf0c6ada94f32a6152e6a755fc
xiaochun <90xchun@gmail.com>
2026-01-14 18:24:54 +0800

    config static page plugin

--- zrlog-blog-web/src/main/java/com/zrlog/blog/web/plugin/TemplateDownloadPlugin.java
+++ zrlog-blog-web/src/main/java/com/zrlog/blog/web/plugin/TemplateDownloadPlugin.java
@@ -3,7 +3,7 @@ package com.zrlog.blog.web.plugin;
 import com.hibegin.common.BaseLockObject;
 import com.hibegin.common.util.EnvKit;
 import com.hibegin.common.util.LoggerUtil;
-import com.zrlog.business.util.TemplateDownloadUtils;
+import com.zrlog.business.template.util.TemplateDownloadUtils;
 import com.zrlog.common.Constants;
 import com.zrlog.plugin.IPlugin;
 
@@ -40,6 +40,9 @@ public class TemplateDownloadPlugin extends BaseLockObject implements IPlugin {
 
     public void precheckTemplate(String templatePath) {
         if (Objects.equals(templatePath, Constants.DEFAULT_TEMPLATE_PATH)) {
+            return;
+        }
+        if (Objects.equals(templatePath, Constants.DEFAULT_HEXO_TEMPLATE_PATH)) {
             return;
         }
         lock.lock();
