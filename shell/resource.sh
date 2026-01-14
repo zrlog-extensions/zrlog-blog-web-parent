11748dab0c955e3f3b5fbbc956a4be95b8fc7476
xiaochun <90xchun@gmail.com>
2026-01-14 15:22:32 +0800

    add ejs template

--- shell/resource.sh
+++ shell/resource.sh
@@ -1,9 +1,11 @@
+basePwd=$(pwd)
 webResourcePath=$(pwd)/zrlog-blog-web/src/main/resources
 templateResourcePath=$(pwd)/zrlog-freemarker-template/src/main/resources
 hexoTemplateResourcePath=$(pwd)/zrlog-hexo-template/src/main/resources
 cd ${webResourcePath}
-find "." -type f  | sed 's|^\./||' > resource.txt
+find "." -type f  | sed 's|^\./||' > ${basePwd}/temp.txt
 cd ${templateResourcePath}
-find "." -type f  | sed 's|^\./||' >> ${webResourcePath}/resource.txt
+find "." -type f  | sed 's|^\./||' >> ${basePwd}/temp.txt
 cd ${hexoTemplateResourcePath}
-find "." -type f  | sed 's|^\./||' >> ${webResourcePath}/resource.txt
+find "." -type f  | sed 's|^\./||' >> ${basePwd}/temp.txt
+sort ${basePwd}/temp.txt > ${webResourcePath}/resource.txt
