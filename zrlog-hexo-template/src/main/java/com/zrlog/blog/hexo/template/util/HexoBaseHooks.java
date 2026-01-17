611ce813661bc8a6f5357d6f46d57986a3b6c057
xiaochun <90xchun@gmail.com>
2026-01-17 19:19:51 +0800

    Optimize partial

--- zrlog-hexo-template/src/main/java/com/zrlog/blog/hexo/template/util/HexoBaseHooks.java
+++ zrlog-hexo-template/src/main/java/com/zrlog/blog/hexo/template/util/HexoBaseHooks.java
@@ -39,7 +39,7 @@ public class HexoBaseHooks {
             String path = args[0].asString();
             Map<String, Object> locals = (args.length > 1 && !args[1].isNull()) ? args[1].as(Map.class) : hexoTemplate.getLocals();
             try {
-                return hexoHelper.partial(path, locals);
+                return hexoTemplate.doRender(path, locals);
             } catch (Exception e) {
                 /*e.fillInStackTrace();
                 throw new RuntimeException(e);*/
