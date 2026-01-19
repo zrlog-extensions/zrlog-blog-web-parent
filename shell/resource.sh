cf884e936f3710f1714ed3a3669a11ed845d0365
xiaochun <90xchun@gmail.com>
2026-01-19 14:43:46 +0800

    config static page plugin

--- shell/resource.sh
+++ shell/resource.sh
@@ -1,12 +1,12 @@
 basePwd=$(pwd)
 webResourcePath=$(pwd)/zrlog-blog-web/src/main/resources
 templateResourcePath=$(pwd)/zrlog-freemarker-template/src/main/resources
-hexoTemplateResourcePath=$(pwd)/zrlog-hexo-template/src/main/resources
+polyglotTemplateResourcePath=$(pwd)/zrlog-polyglot-template/src/main/resources
 cd ${webResourcePath}
 find "." -type f  | sed 's|^\./||' > ${basePwd}/temp.txt
 cd ${templateResourcePath}
 find "." -type f  | sed 's|^\./||' >> ${basePwd}/temp.txt
-cd ${hexoTemplateResourcePath}
+cd ${polyglotTemplateResourcePath}
 find "." -type f  | sed 's|^\./||' >> ${basePwd}/temp.txt
 export LC_ALL=C
 sort ${basePwd}/temp.txt > ${webResourcePath}/resource.txt
